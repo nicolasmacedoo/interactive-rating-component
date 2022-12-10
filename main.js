7cc196fc90959b3819dd2c0922ededfb76c159a1
NicolasMacedoo <nicolas.conceicao01@gmail.com>
2022-12-10 16:40:00 -0300

    ratings and submit button funcion reftored

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 import './style.css'
 
 const rating = document.querySelectorAll('.rate')
+const btnSubmit = document.querySelector('#btn-submit')
+const card = document.querySelector('.card')
+const thankYou = document.querySelector('.thank-you')
+const rated = document.querySelector('.rated')
+let rate = ''
 
-for (let i = 0; i < rating.length; i++) {
-  rating[i].addEventListener('click', () => {
-    rating[i].classList.toggle('active')
-  });
-}
+rating.forEach(num => {
+  num.addEventListener('click', () => {
+    rating.forEach(selected => selected.classList.remove('active'))
+    num.classList.toggle('active')
+    rate = num.textContent
+  })
+})
+
+//ao inves de esconder(hidden) usar JS para inner html na div card com as novas informacoes
+
+btnSubmit.addEventListener('click', () => {
+   rated.innerHTML = `You selected ${rate} out of 5`
+   card.classList.add('hidden')
+   thankYou.classList.remove('hidden')
+})
