d6ab5812b1de6bc48e594f84582bd9be7d59881d
NicolasMacedoo <nicolas.conceicao01@gmail.com>
2022-12-10 15:32:40 -0300

    refactored main.js

--- main.js
+++ main.js
@@ -1,36 +1,9 @@
 import './style.css'
 
-const rate1 = document.querySelector('#one')
+const rating = document.querySelectorAll('.rate')
 
-rate1.addEventListener('click', () => {
-  console.log(rate1.textContent)
-  rate1.classList.toggle('active')
-})
-
-const rate2 = document.querySelector('#two')
-
-rate2.addEventListener('click', () => {
-  console.log(rate2.textContent)
-  rate2.classList.toggle('active')
-})
-
-const rate3 = document.querySelector('#three')
-
-rate3.addEventListener('click', () => {
-  console.log(rate3.textContent)
-  rate3.classList.toggle('active')
-})
-
-const rate4 = document.querySelector('#four')
-
-rate4.addEventListener('click', () => {
-  console.log(rate4.textContent)
-  rate4.classList.toggle('active')
-})
-
-const rate5 = document.querySelector('#five')
-
-rate5.addEventListener('click', () => {
-  console.log(rate5.textContent)
-  rate5.classList.toggle('active')
-})
+for (let i = 0; i < rating.length; i++) {
+  rating[i].addEventListener('click', () => {
+    rating[i].classList.toggle('active')
+  });
+}
